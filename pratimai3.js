cd81f9155d7046fe83365bbca7bf7775e6afd2c5
Mnde81 <minubis@gmail.com>
2024-06-06 11:35:00 +0300

    updated file

--- pratimai3.js
+++ pratimai3.js
@@ -167,5 +167,140 @@ console.log(dalyba(false, 4));
 console.log(dalyba(-100, 20));
 console.log(dalyba(55, 4));
 
-// console.clear();
+console.clear();
+
+
+const B = [
+    'the quick brown fox',
+    'jumps over the lazy dog',
+    'a bird in the hand',
+    'is worth two in the bush',
+    'an apple a day',
+    'keeps the doctor away',
+    'actions speak louder than words',
+    'all good things must come to an end',
+    'beauty is in the eye of the beholder',
+    'better late than never',
+    'birds of a feather flock together',
+    'cleanliness is next to godliness',
+    'dont bite the hand that feeds you',
+    'dont count your chickens before they hatch',
+    'dont put all your eggs in 1 basket',
+    'every cloud has a silver lining',
+    'every dog has its day',
+    'every rose has its thorn',
+    'fortune favors the bold',
+    'good things come to those who wait',
+    'a watched pot never boils',
+    'haste makes waste',
+    'history repeats itself',
+    'cat got your tongue',
+    'home is where the heart is',
+    'if the shoe fits, wear it',
+    'ignorance is bliss',
+    'it takes two to tango',
+    'arabs have 100 words for sand',
+    'kill two birds with one stone',
+    'laughter is the best medicine',
+    'love is blind',
+    'money doesnt grow on trees',
+    'no pain no gain',
+    'nothing ventured nothing gained',
+    'once bitten twice shy',
+    'practice makes perfect',
+    'the early bird catches the worm',
+    'the grass is always greener on the other side',
+    'the pen is mightier than 10 swords',
+    'theres no smoke without fire',
+    'time heals all wounds',
+    '5 second rule',
+    'every dog has its day',
+    'to err is human to forgive divine',
+    '2 wrongs dont make a right',
+    'when in Rome do as the Romans do',
+    'where theres smoke, theres fire',
+    'you cant judge a book by its cover',
+    'you cant make an omelette without breaking eggs',
+    'you reap what you sow',
+    'youre never too old to learn'
+];
+
+// Surasti ilgiausią stringą;
+
+function longestString(arr) {
+    let string = '';
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length > string.lenght) {
+            string = arr[i];
+        }
+    }
+    return string;
+}
+
+
+
+// Surasti trumpiausią stringą;
+
+function shortestString(arr) {
+    let string = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length < string.lenght) {
+            string = arr[i];
+        }
+    }
+    return string;
+}
+
+
+// Surasti stringą, kuris prasideda “a” raide;
+
+function startWithA(arr) {
+    let string = '';
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].startsWith('a')) {
+            string = arr[i];
+            break;
+        }
+    }
+    return string;
+}
+
+
+
+// Surasti stringą su daugiausia žodžių;
+
+function longest(array) {
+    let stringLongest = '';
+    let maxWordsCount = 0;
+    for (let i = 0; i < array.length; i++) {
+        let wordCount = array[i].split(' ').length;
+
+        if (wordCount > maxWordsCount) {
+            maxWordsCount = wordCount;
+            stringLongest = array[i];
+        }
+    }
+    return stringLongest;
+}
+// Surasti stringą su mažiausiai žodžių;
+
+function shortest(array) {
+    let shortest = array[0];
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].split(' ').length < shortest.split(' ').length) {
+            shortest = array[i];
+        }
+    }
+    return shortest;
+}
+
+
+
+
+// Suskaičiuoti kiek stringų turi daugiau nei 4 žodžius;
+// Suskaičiuoti kiek masyve yra žodžių;
+// Suskaičiuoti visas ‘s’ raides;
+// Suskaičiuoti kiek masyve yra raidžių (tarpų tarp žodžių neskaičiuoti!);
+// Surasti ilgiausią žodį (ne stringą, o atskirą stringo žodį!);
 
